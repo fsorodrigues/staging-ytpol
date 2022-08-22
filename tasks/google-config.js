80381c1caef511db50a0a901e8778c9bb70c2c65
Felippe Rodrigues <fsorodrigues@gmail.com>
2022-08-22 17:58:07 +1200

    Unique copy doc

--- tasks/google-config.js
+++ tasks/google-config.js
@@ -1,14 +1,8 @@
 const docs = [
 	{
-		"id": "1JRCXNg6ZQVHRZtVJibedJjJWb_Nq-pUKimwhykWYzoA",
-		"filepath": "src/data/copy.json"
-	},
-	{
 		"id": "1MxaNGzySt2i1IBPZ4ZH9_ZtRKSTPOHro-9B0sok3OVA",
-		"filepath": "src/data/copy_2.json"
-	},
-
-	
+		"filepath": "src/data/copy.json"
+	}
 ]
 
 module.exports = docs
