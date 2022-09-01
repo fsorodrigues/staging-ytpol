30f9f30d1d608e56a5b5ed5e633c1b13d293145e
Felippe Rodrigues <fsorodrigues@gmail.com>
2022-09-02 09:30:26 +1200

    Change color scheme

--- src/utils/colors.ts
+++ src/utils/colors.ts
@@ -7,14 +7,13 @@ const colorMap : Map<string, string> = new Map([
     ["fR", "#AC3535"],
 ])
 
-// ffe0ad-ffd085-ffb133-e08a00-ffc05c-f4a261-e9c46a-ffa10a-e76f51
 const youTubeMap : Map<string, string> = new Map([
     ["Ext URL", "#E08A00"],
-    ["HP", "#FFC05C"],
-    ["Other", "#FFA10A"],
-    ["Search", "#E9C46A"],
-    ["User/Channel", "#F4A261"],
-    ["Video", "#E76F51"]
+    ["HP", "#e4a33c"],
+    ["Other", "#e9c995"],
+    ["Search", "#a7c2c0"],
+    ["User/Channel", "#609692"],
+    ["Video", "#317873"]
 ])
 
 export default colorMap;
